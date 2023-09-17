746a07d478e152384dd7cec61facd1380fa4336a
homosapian <ankitd349@gmail.com>
2023-09-17 22:50:02 +0530

    Static generation pre-rendering

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import React from "react";
+import fs from "fs/promises";
+import Link from "next/link";
+import path from "path";
 
-const HomePage = () => {
+const HomePage = (props) => {
+  const { products } = props;
   return (
-    <div>
-      <h1>Welcome to the Future</h1>
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
+export async function getStaticProps() {
+  // cwd => current work directory and it takes the root path;
+  //await in readFile gives a promise and it is possible due to fs/promises;
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
 export default HomePage;
